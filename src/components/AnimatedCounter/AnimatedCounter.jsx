7eb01def7e8e1ed8909f120295723727766de67d
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-27 20:39:24 +0500

    ASD

--- src/components/AnimatedCounter/AnimatedCounter.jsx
+++ src/components/AnimatedCounter/AnimatedCounter.jsx
@@ -7,7 +7,7 @@ const AnimatedCounter = ({ amount, loading }) => {
             {loading ? (
                 <span className="w-full min-w-[80%] skeleton h-6 bg-gray-300 animate-pulse"></span>
             ) : (
-                <span className='pr-2'>
+                <span className='pr-2 text-xl lg:text-2xl'>
                     <CountUp
                         end={amount}
                         duration={0.675}
@@ -17,7 +17,7 @@ const AnimatedCounter = ({ amount, loading }) => {
                     />
                 </span>
             )}
-            <span>UZS</span>
+            <span className='text-xl lg:text-2xl'>UZS</span>
         </div>
     )
 }
