bde7f2047028a57ccff7d1088f25cd12c3f0bfd3
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-10-31 16:53:19 +0500

    AS

--- src/components/HeaderBox/HeaderBox.jsx
+++ src/components/HeaderBox/HeaderBox.jsx
@@ -6,7 +6,7 @@ const HeaderBox = ({ type = "title", title, subtext, user }) => {
       <h1 className="header-box-title">
         {title}
         {type === 'greeting' && (
-          <span className="text-bankGradient">
+          <span className="2xl:text-26 font-ibm-plex-serif text-[26px] font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#333c5f] to-[#1dbef5]">
             &nbsp;{user}
           </span>
         )}
